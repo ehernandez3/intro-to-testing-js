90162f1ce822c43c03a00c96c346ca1b73600cf8
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2019-03-28 22:40:19 -0500

    add the first refactoring solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -23,7 +23,7 @@ describe('sayHello', function() {
         expect(typeof sayHello()).toBe('string');
     });
     it('should return "Hello, Jane!" when calling sayHello("Jane")', function() {
-        expect(sayHello()).toBe("Hello, Jane!")
+        expect(sayHello("Jane")).toBe("Hello, Jane!")
     });
     it('should return "Hello, Alex!" when calling sayHello("Alex")', function() {
         expect(sayHello("Alex")).toBe("Hello, Alex!");
